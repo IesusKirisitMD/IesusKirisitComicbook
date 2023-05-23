4ff6870003db9395ac51cd15f3a4fcb67a2008ff
IesusKirisitMD <iesuskirisit@gmail.com>
2023-05-23 15:05:22 -0400

    yo

---
+++ classwork/app.js
@@ -0,0 +1,18 @@
+/**
+ * // Classroom Behaviors
+ * 
+ * // Classroom Guidelines
+ * 
+ * //Repl.it vs CodeSandbox
+ * 
+ * //VSCODE and Github and Terminal
+ *   //-> Extensions
+ *   //-> Git Process
+ *   //-> 
+ * 
+ * 
+ * 
+ * 
+ */
+// Javascript file 
+//<--the functions-->
